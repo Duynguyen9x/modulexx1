351ccad7ccbb1c34536e2869c2ff94a312c7b13c
Duynguyen9x <topcoder9x@gmail.com>
2016-12-28 21:14:59 +0700

    Add two sub Fragment into PhotoFragment

---
+++ app/src/main/java/info/androidhive/navigationdrawer/adapter/BookTheoryAdapter.java
@@ -0,0 +1,78 @@
+package info.androidhive.navigationdrawer.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import info.androidhive.navigationdrawer.R;
+import info.androidhive.navigationdrawer.model.BookTheory;
+import info.androidhive.navigationdrawer.utils.ImageUtils;
+
+/**
+ * Created by 8470p on 12/28/2016.
+ */
+public class BookTheoryAdapter extends ArrayAdapter<BookTheory> {
+    ArrayList<BookTheory> mArrayList;
+    Context mContext;
+
+    public BookTheoryAdapter(Context context, int resource, ArrayList<BookTheory> mArrayList) {
+        super(context, resource, mArrayList);
+        this.mContext = context;
+        this.mArrayList = mArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return ((null != mArrayList) ? mArrayList.size() : 0);
+    }
+
+    @Override
+    public BookTheory getItem(int position) {
+        return ((null != mArrayList) ? mArrayList.get(position) : null);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        BookTheory bookTheory = getItem(position);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.booktheory_item_layout, null);
+
+            holder = new ViewHolder();
+
+            holder.number = (TextView) convertView.findViewById(R.id.number);
+            holder.content = (TextView) convertView.findViewById(R.id.contents);
+
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        if (null != bookTheory) {
+            holder.number.setText(bookTheory.getNumber());
+            holder.content.setText(bookTheory.getContent());
+        }
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        public TextView number;
+        public TextView content;
+    }
+}
